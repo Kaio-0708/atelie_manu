52678bea347db508bdc76e150aedfcd14a9bb379
Kaio0708 <kaiovitor0707@gmail.com>
2025-10-17 19:39:15 -0300

    Ajuste de pacotes

--- api/server.js
+++ api/server.js
@@ -48,3 +48,8 @@ app.post("/api/create-checkout-session", async (req, res) => {
 });
 
 module.exports = app;
+
+/*const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+  console.log(`Servidor rodando na porta ${PORT}`);
+});*/
